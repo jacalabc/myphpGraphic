aab8daa4bd9f96479f5088c1a6c8c8012f2c121b
jerry <jerry19971228@gmail.com>
2023-02-24 14:42:26 +0800

    使用php製作縮圖

--- text-import.php
+++ text-import.php
@@ -1,4 +1,5 @@
 <?php
+include_once "./api/base.php";
 /****
  * 1.建立資料庫及資料表
  * 2.建立上傳檔案機制
@@ -8,6 +9,26 @@
  * 6.寫入資料庫
  * 7.結束檔案
  */
+
+
+    if(!empty($_FILES['doc']['tmp_name'])){
+        //move_uploaded_file($_FILES['doc']['tmp_name'], './upload/'.$_FILES['doc']['name']);
+        $file=$_FILES['doc']['tmp_name'];
+    
+        $resource=fopen($file,'r');
+        $line=fgets($resource);
+        while($line=fgets($resource)){
+            $cols=explode(",",$line);
+            $cols[2]=trim(trim($cols[2]),"'");
+            $cols[3]=trim(trim($cols[3]),"'");
+            $cols[4]=trim(trim($cols[4]),"'");
+            insert('students',['name'=>$cols[2],
+                                'no'=>$cols[4],
+                                'birthday'=>$cols[3]]);
+        }
+
+        unlink($_FILES['doc']['tmp_name']);
+    }
 
 ?>
 <!DOCTYPE html>
@@ -21,12 +42,50 @@
 </head>
 <body>
 <h1 class="header">文字檔案匯入練習</h1>
-<!---建立檔案上傳機制--->
+<form action="?" method='post' enctype="multipart/form-data">
+    <input type="file" name="doc" id="doc">
+    <input type="submit" value="上傳">
+
+</form>
+
 
 
 
 <!----讀出匯入完成的資料----->
+<?php
+$rows=all('students');
+foreach($rows as $row){
+    echo $row['name']. "-".$row['no']."-".$row['birthday'];
+    echo "<br>";
+}
 
+?>
+
+<button onclick="location.href='?do=export'">匯出資料</button>
+
+<!--匯出資料-->
+<?php
+
+if(isset($_GET['do']) && $_GET['do']=='export'){
+    if(file_exists('export.csv')){
+        unlink('export.csv');
+    }else{
+        $file=fopen('export.csv','w+');
+        $rows=all('students'," limit 5");
+        fwrite($file,"\xEF\xBB\xBF");
+
+        foreach($rows as $row){
+            $str=join(",",$row);
+            fwrite($file,$str);
+            fwrite($file,"\r\n");
+        }
+
+        fclose($file);
+        echo "<a href='export.csv' download>檔案下載</a>";
+
+    }
+}
+?>
 
 
 </body>
